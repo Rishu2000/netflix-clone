f27884abe46ce4c70398955ade5988a35b729c8f
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-08-29 19:53:11 +0530

    Get the movie for the banner.

---
+++ src/components/Banner.js
@@ -0,0 +1,27 @@
+import React, {useState, useEffect} from 'react'
+import axios from "../axios"
+import requests from "../requests"
+
+const Banner = () => {
+
+const [movie, setMovie] = useState([]);
+
+useEffect(() => {
+    async function fetchData() {
+        const request = await axios.get(requests.fetchNetflixOriginals);
+        setMovie(request.data.results[Math.floor(Math.random() * request.data.results.length - 1)]);
+    }
+    fetchData();
+    // console.log(movie);
+}, [])
+
+    return (
+        <header> {/** Background image */}
+            {/* title */}
+            {/* div > 2 buttons */}
+            {/* description */}
+        </header>
+    )
+}
+
+export default Banner
