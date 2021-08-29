a9c09206f64c68a918c1eada992348e435ffadad
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-08-29 12:37:22 +0530

    fetchData function is dependent on fetchUrl data hence, need to include it.

--- src/components/Row.js
+++ src/components/Row.js
@@ -8,13 +8,13 @@ const [movies, setMovies] = useState([]);
 // A snippet of code which runs based on a specific conditions.
 useEffect(() => {
     async function fetchData() {
-        const request = await axios.get(fetchUrl);
+        const request = await axios.get(fetchUrl);      //This function is dependent on fetchUrl.
         // console.log(request.data.results);
         setMovies(request.data.results);
         return request;
     }
     fetchData();
-},[])
+},[fetchUrl])
 
     return (
         <div>
