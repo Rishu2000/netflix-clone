7eb936dd156751105ff4b54bc56d0cd25eb1f2a9
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-08-29 12:35:41 +0530

    fetching data using axios is complete.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import React from 'react'
 import './App.css';
+import Row from "./components/Row"
+import requests from './requests'
 
 function App() {
   return (
     <div className="App">
       <h1>Rishav</h1>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
     </div>
   );
 }
---
+++ src/components/Row.js
@@ -0,0 +1,28 @@
+import React, {useState, useEffect} from 'react'
+import axios from '../axios';
+
+const Row = ({title, fetchUrl}) => {
+
+const [movies, setMovies] = useState([]);
+
+// A snippet of code which runs based on a specific conditions.
+useEffect(() => {
+    async function fetchData() {
+        const request = await axios.get(fetchUrl);
+        // console.log(request.data.results);
+        setMovies(request.data.results);
+        return request;
+    }
+    fetchData();
+},[])
+
+    return (
+        <div>
+            {/* title */}
+            <h2>{title}</h2>
+            {/* container -> posters */}
+        </div>
+    )
+}
+
+export default Row
