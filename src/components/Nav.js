bb548f9b7a9a82161cd7cb039f9d9ad7971c1674
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-08-30 13:37:31 +0530

    Added navbar and it's css styling.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,8 +3,17 @@ import "../styles/Nav.css"
 
 const Nav = () => {
     return (
-        <div>
-            Nav   
+        <div className="nav">
+            <img 
+                className="nav_logo"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Netflix_2014_logo.svg/450px-Netflix_2014_logo.svg.png"
+                alt="Netflix Logo"
+            />
+            <img 
+                className="nav_avatar"
+                src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+                alt="Netflix Logo"
+            />
         </div>
     )
 }
