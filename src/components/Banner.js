ad5ba259a18699a9bd5644f2d13981512afcdb5a
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-08-30 11:32:01 +0530

    Added the button styling.

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -16,7 +16,7 @@ useEffect(() => {
 }, [])
 
 const truncate = (str,n) => {
-    return (str?.length > n ? str.substr(0,n-1) + "...": str);
+    return (str?.length > n ? str.substr(0,n-1) + " ...": str);
 }
 
     return (
